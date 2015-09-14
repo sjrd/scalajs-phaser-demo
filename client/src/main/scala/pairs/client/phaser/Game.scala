88afa42b05a8c032cb4ceb473a7cac8685c05396
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-14 17:31:30 +0200

    Add an empty Phase Game to the page.
    This illustrates:
    * Native classes and their constructors
    * Native objects
    * val field in a native object

---
+++ client/src/main/scala/pairs/client/phaser/Game.scala
@@ -0,0 +1,11 @@
+package pairs.client.phaser
+
+import scala.scalajs.js
+import js.annotation._
+
+@js.native
+@JSName("Phaser.Game")
+class Game(width: Double, height: Double, renderer: Int,
+    parent: String) extends js.Object {
+
+}
