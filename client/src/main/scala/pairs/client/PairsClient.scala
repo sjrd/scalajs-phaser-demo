c2ef67055bc06b4aa8b7d4ed651754a7ff248289
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-14 18:02:47 +0200

    Add an empty game State.
    This illustrates:
    * methods in native classes
    * extending a native class from Scala.js

--- client/src/main/scala/pairs/client/PairsClient.scala
+++ client/src/main/scala/pairs/client/PairsClient.scala
@@ -1,12 +1,19 @@
 package pairs.client
 
 import scala.scalajs.js
+import js.annotation._
 import org.scalajs.dom
 
 import pairs.client.phaser._
 
+class GameState extends State {
+
+}
+
 object PairsClient {
   def main(args: Array[String]): Unit = {
     val game = new Game(width = 800, height = 520, parent = "pairs-container")
+    game.state.add("game", new GameState)
+    game.state.start("game")
   }
 }
