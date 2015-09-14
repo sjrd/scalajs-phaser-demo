19208d953ef770dd9499a0fb8c9f684e59472f44
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-14 18:02:47 +0200

    Add an empty game State.
    This illustrates:
    * methods in native classes
    * extending a native class from Scala.js

--- client/src/main/scala/pairs/client/phaser/Phaser.scala
+++ client/src/main/scala/pairs/client/phaser/Phaser.scala
@@ -18,4 +18,20 @@ class Game(
     renderer: Int = Phaser.AUTO,
     parent: String | html.Element = "") extends js.Object {
 
+  val state: StateManager = js.native
+}
+
+@js.native
+@JSName("Phaser.StateManager")
+class StateManager(val game: Game) extends js.Object {
+  def add(key: String, state: State,
+      autoStart: Boolean = false): Unit = js.native
+
+  def start(key: String): Unit = js.native
+}
+
+@js.native
+@JSName("Phaser.State")
+abstract class State extends js.Object {
+
 }
