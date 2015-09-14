1c8be3277d69cbc66316a44b076a838837e0ad06
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-14 17:50:38 +0200

    Use default parameters in the Game constructor.
    This illustrates:
    * Default parameters in native JS types

--- client/src/main/scala/pairs/client/phaser/Phaser.scala
+++ client/src/main/scala/pairs/client/phaser/Phaser.scala
@@ -14,9 +14,9 @@ object Phaser extends js.Object {
 @js.native
 @JSGlobal("Phaser.Game")
 class Game(
-    width: Double | String,
-    height: Double | String,
-    renderer: Int,
-    parent: String | html.Element) extends js.Object {
+    width: Double | String = 800,
+    height: Double | String = 600,
+    renderer: Int = Phaser.AUTO,
+    parent: String | html.Element = "") extends js.Object {
 
 }
