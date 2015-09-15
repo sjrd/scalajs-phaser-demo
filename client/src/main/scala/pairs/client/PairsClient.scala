d9995a571e59101c8ac8255b8ecce49d2a4b2f95
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-15 15:38:11 +0200

    Finally, the game logic.

--- client/src/main/scala/pairs/client/PairsClient.scala
+++ client/src/main/scala/pairs/client/PairsClient.scala
@@ -11,6 +11,9 @@ class Square(val row: Int, val col: Int, val card: Int,
 
 @ScalaJSDefined
 class GameState extends State {
+  private var firstClick: Option[Square] = None
+  private var secondClick: Option[Square] = None
+
   override def preload(): Unit = {
     load.image("back", "assets/back.png")
     for (i <- 0 to 9)
@@ -42,6 +45,33 @@ class GameState extends State {
   }
 
   private def doClick(square: Square): Unit = {
+    (firstClick, secondClick) match {
+      case (None, _) =>
+        // First click of a pair
+        firstClick = Some(square)
+
+      case (Some(first), None) if first.card == square.card =>
+        // Found a pair
+        firstClick = None
+
+      case (Some(_), None) =>
+        // Missing a pair, need to hide it later
+        secondClick = Some(square)
+        js.timers.setTimeout(1000) {
+          assert(firstClick.isDefined && secondClick.isDefined)
+          for (square <- Seq(firstClick.get, secondClick.get)) {
+            square.front.visible = false
+            square.back.visible = true
+          }
+          firstClick = None
+          secondClick = None
+        }
+
+      case (Some(_), Some(_)) =>
+        // Third click, cancel (have to wait for the deadline to elapse)
+        return
+    }
+
     square.back.visible = false
     square.front.visible = true
   }
