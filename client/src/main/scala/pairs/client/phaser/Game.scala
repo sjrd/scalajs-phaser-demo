cf6667afa113ce6a88ba98040a5e159b861d50e2
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-16 11:44:13 +0200

    Accept String | html.Element in the Game constructor.
    This illustrates:
    * A | B

--- client/src/main/scala/pairs/client/phaser/Game.scala
+++ client/src/main/scala/pairs/client/phaser/Game.scala
@@ -2,11 +2,14 @@ package pairs.client.phaser
 
 import scala.scalajs.js
 import js.annotation._
+import js.|
+
+import org.scalajs.dom.html
 
 @js.native
 @JSName("Phaser.Game")
 class Game(width: Double = 800, height: Double = 600,
     renderer: Int = Phaser.AUTO,
-    parent: String = "") extends js.Object {
+    parent: String | html.Element = "") extends js.Object {
 
 }
