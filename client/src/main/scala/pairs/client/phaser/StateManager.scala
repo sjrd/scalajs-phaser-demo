38f224be72b32505f0bf0f57fa5c7e4f3bf5258d
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-16 13:01:59 +0200

    Use js.constructorOf[GameState]  instead of new GameState.
    This illustrates:
    * js.constructorOf
    
    It's actually not a good idea, here, but it allows to show off
    js.constructorOf.

--- client/src/main/scala/pairs/client/phaser/StateManager.scala
+++ client/src/main/scala/pairs/client/phaser/StateManager.scala
@@ -2,11 +2,12 @@ package pairs.client.phaser
 
 import scala.scalajs.js
 import js.annotation._
+import js.|
 
 @js.native
 @JSName("Phaser.StateManager")
 class StateManager(val game: Game) extends js.Object {
-  def add(key: String, state: State,
+  def add(key: String, state: State | js.Dynamic,
       autoStart: Boolean = false): Unit = js.native
 
   def start(key: String): Unit = js.native
