eb20351102dd284ca83df43d2b25a4e180dd6cb5
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-09-16 17:39:58 +0200

    Use Phaser's Point class instead of our literals.
    This illustrates:
    * A native JS class can extend a Scala.js-defined JS trait

---
+++ client/src/main/scala/pairs/client/phaser/Point.scala
@@ -0,0 +1,8 @@
+package pairs.client.phaser
+
+import scala.scalajs.js
+import js.annotation._
+
+@js.native
+@JSName("Phaser.Point")
+class Point(var x: Double = 0, var y: Double = 0) extends PointLike
