6573e02e46919073b4d82b10674a47067ac5c937
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-11-06 16:53:04 +0100

    Upgrades.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 val commonSettings = Seq(
   version := "0.1.0-SNAPSHOT",
-  scalaVersion := "2.11.7",
+  scalaVersion := "2.12.4",
   scalacOptions ++= Seq("-deprecation", "-feature", "-encoding", "utf-8")
 )
 
@@ -8,8 +8,9 @@ lazy val `pairs-client` = project.in(file("client")).
   enablePlugins(ScalaJSPlugin).
   settings(commonSettings).
   settings(
+    scalacOptions += "-P:scalajs:sjsDefinedByDefault",
     libraryDependencies ++= Seq(
-      "org.scala-js" %%% "scalajs-dom" % "0.8.1"
+      "org.scala-js" %%% "scalajs-dom" % "0.9.3"
     ),
     persistLauncher in Compile := true,
     persistLauncher in Test := false
@@ -20,8 +21,9 @@ lazy val `pairs-server` = project.in(file("server")).
   settings(Revolver.settings).
   settings(
     libraryDependencies ++= Seq(
-      "io.spray" %% "spray-can" % "1.3.3",
-      "io.spray" %% "spray-routing" % "1.3.3",
-      "com.typesafe.akka" %% "akka-actor" % "2.3.9"
+      "org.scala-lang.modules" %% "scala-xml" % "1.0.6",
+      "com.typesafe.akka" %% "akka-http" % "10.0.10",
+      "com.typesafe.akka" %% "akka-actor" % "2.5.4",
+      "com.typesafe.akka" %% "akka-stream" % "2.5.4"
     )
   )
--- server/src/main/scala/pairs/server/Server.scala
+++ server/src/main/scala/pairs/server/Server.scala
@@ -1,13 +1,16 @@
 package pairs.server
 
 import akka.actor.ActorSystem
-import spray.http.{HttpEntity, MediaTypes}
-import spray.routing.SimpleRoutingApp
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
 
 import scala.util.Properties
 
-object Server extends SimpleRoutingApp {
-  private val Index = {
+object Server {
+  private val Index = """
     <html>
       <head>
         <title>Pairs</title>
@@ -20,29 +23,36 @@ object Server extends SimpleRoutingApp {
         <script type="application/javascript" src="/js/pairs-client-launcher.js"></script>
       </body>
     </html>
-  }
+  """
 
   def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem()
-    val port = Properties.envOrElse("PORT", "8080").toInt
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
 
-    startServer("0.0.0.0", port = port) {
-      get {
-        pathSingleSlash {
-          respondWithMediaType(MediaTypes.`text/html`) {
-            complete {
-              Index
-            }
-          }
-        } ~
-        getFromResourceDirectory("")
-      } ~
-      path("js" / Rest) { fileName =>
-        getFromFile("../client/target/scala-2.11/" + fileName)
+    val route = {
+      pathPrefix("js") { //fileName =>
+        //getFromFile("../client/target/scala-2.11/" + fileName)
+        getFromDirectory("./client/target/scala-2.12")
       } ~
       pathPrefix("assets") {
         getFromResourceDirectory("assets")
+      } ~
+      pathSingleSlash {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, Index))
+        } ~
+        getFromResourceDirectory("")
       }
     }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 }
