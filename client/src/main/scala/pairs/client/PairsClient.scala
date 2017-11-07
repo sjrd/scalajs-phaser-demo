c2cf5dcf1f3debfddadbe3e171dd7539e16918de
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-11-07 12:11:58 +0100

    New version of the talk.

--- client/src/main/scala/pairs/client/PairsClient.scala
+++ client/src/main/scala/pairs/client/PairsClient.scala
@@ -3,10 +3,111 @@ package pairs.client
 import scala.scalajs.js
 import org.scalajs.dom
 
+import pairs.client.phaser._
+
+object Game {
+  final val TileSize = 130
+
+  sealed abstract class GuessState
+  case object NoGuess extends GuessState
+  case class OneCard(index: Int) extends GuessState
+  case object WrongGuess extends GuessState
+}
+
+class Game extends Phaser.State {
+  import Game._
+
+  private var images: IndexedSeq[Phaser.Sprite] = _
+  private var cards: IndexedSeq[Phaser.Sprite] = _
+  private var score: Int = 0
+
+  def preload(): Unit = {
+    load.image("back", "assets/back.png")
+    for (i <- 0 until 10)
+      load.image(i.toString, s"assets/$i.png")
+  }
+
+  def create(): Unit = {
+    val allCardNumbers = (0 until 10) ++ (0 until 10)
+    val imagesNotShuffled = allCardNumbers.map { i =>
+      game.add.sprite(0, 0, i.toString())
+    }
+    images = scala.util.Random.shuffle(imagesNotShuffled)
+    cards = images.map { _ =>
+      game.add.sprite(0, 0, "back")
+    }
+
+    for (i <- 0 until 4; j <- 0 until 5) {
+      val idx = i*5 + j
+      val left = j * TileSize
+      val top = i * TileSize
+
+      val image = images(idx)
+      image.x = left
+      image.y = top
+      image.visible = false
+
+      val card = cards(idx)
+      card.x = left
+      card.y = top
+      card.inputEnabled = true
+      card.events.onInputDown.add(() => doClick(idx))
+      card.events.onInputOver.add { () =>
+        card.alpha = 0.5
+      }
+      card.events.onInputOut.add { () =>
+        card.alpha = 1.0
+      }
+    }
+  }
+
+  private var guessState: GuessState = NoGuess
+
+  private def doClick(idx: Int): Unit = {
+    val card = cards(idx)
+    val image = images(idx)
+
+    guessState match {
+      case NoGuess =>
+        guessState = OneCard(idx)
+
+      case OneCard(first) =>
+        if (images(first).key == images(idx).key) {
+          score += 50
+          guessState = NoGuess
+        } else {
+          score -= 5
+          guessState = WrongGuess
+          js.timers.setTimeout(1000) {
+            images(first).visible = false
+            images(idx).visible = false
+            cards(first).visible = true
+            cards(idx).visible = true
+            guessState = NoGuess
+          }
+        }
+
+      case WrongGuess =>
+        return
+    }
+
+    card.visible = false
+    image.visible = true
+  }
+
+  def update(): Unit = {}
+
+  def render(): Unit = {
+    game.asInstanceOf[js.Dynamic].debug.text(
+        s"Score: $score", 660, 20)
+  }
+}
+
 object PairsClient {
   def main(args: Array[String]): Unit = {
-    val p = dom.document.createElement("p")
-    p.textContent = "Hello"
-    dom.document.getElementById("pairs-container").appendChild(p)
+    val game = new Phaser.Game(width = 800, height = 520,
+        parent = "pairs-game")
+    game.state.add("game", js.constructorOf[Game])
+    game.state.start("game")
   }
 }
---
+++ client/src/main/scala/pairs/client/phaser/Phaser.scala
@@ -0,0 +1,66 @@
+package pairs.client.phaser
+
+import scala.scalajs.js
+import js.annotation._
+import js.|
+
+@js.native
+@JSGlobal
+object Phaser extends js.Object {
+  val AUTO: Int = js.native
+
+  @js.native
+  class Game(
+      width: Double = 800, height: Double = 600,
+      renderer: Int = AUTO, parent: String = ""
+  ) extends js.Object {
+    val state: StateManager = js.native
+    val add: GameObjectFactory = js.native
+  }
+
+  @js.native
+  class StateManager extends js.Object {
+    def add(name: String, ctor: js.Any): Unit = js.native
+    def start(name: String): Unit = js.native
+  }
+
+  @js.native
+  abstract class State extends js.Object {
+    protected val game: Game = js.native
+    protected val load: Loader = js.native
+  }
+
+  @js.native
+  class Loader extends js.Object {
+    def image(key: String, url: String,
+        overwrite: Boolean = false): Unit = js.native
+  }
+
+  @js.native
+  class GameObjectFactory extends js.Object {
+    def sprite(x: Double, y: Double, key: String): Sprite = js.native
+  }
+
+  @js.native
+  class Sprite extends js.Object {
+    val key: String = js.native
+    var x: Double = js.native
+    var y: Double = js.native
+    var visible: Boolean = js.native
+    var inputEnabled: Boolean = js.native
+    var alpha: Double = js.native
+    val events: Events = js.native
+  }
+
+  @js.native
+  class Events extends js.Object {
+    var onInputDown: Signal = js.native
+    var onInputOver: Signal = js.native
+    var onInputOut: Signal = js.native
+  }
+
+  @js.native
+  class Signal extends js.Object {
+    def add(listener: js.Function): Unit = js.native
+  }
+}
